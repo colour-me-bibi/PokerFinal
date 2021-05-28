32c36d1802bfb28017bf9db5aca6b4f493427d87
colour-me-bibi <bennyforeman1@gmail.com>
2021-05-28 15:15:16 -0700

    documented

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
  * @author Benjamin Foreman (bennyforeman1@gmail.com)
  * @date 2021-04-17
  * 
- * This program determine the winnner from a file of poker hands and
- * play poker with computers who learn through simulation.
+ * This program takes a formatted list of 5 card poker hands from two players
+ * and determines the winner, printing the result to both stdout and csis.txt.
+ * 
+ * The program aims to solve problem 54 of projecteuler.net (https://projecteuler.net/problem=54)
+ * The correct submission for this problem is 376 and this program accurately
+ * arrive to the same conclusion.
  */
 
 #include <stdarg.h>
@@ -19,9 +23,21 @@
 
 const int RANK_COUNT = 15;
 
-// print nicely
-// documentation
-// cleanup
+typedef struct
+{
+    char rank;
+    int value;
+    char suit;
+} Card;
+
+typedef struct
+{
+    int score;
+    size_t play_val_count;
+    int *play_vals;
+    size_t high_val_count;
+    int *high_vals;
+} Play;
 
 /**
  * Prints a formatted message to both stdout and a given file.
@@ -30,6 +46,256 @@ const int RANK_COUNT = 15;
  * @param formatted_message formatted print message
  * @param ... variadic arglist for fromatted printing
  */
+void csis_printf(FILE *fp, const char *formatted_message, ...);
+
+/**
+ * Converts a card rank to the cooresponding integer value.
+ * 
+ * @param rank rank of the card
+ * @return int cooresponding integer value
+ */
+int rank_to_value(char rank);
+
+/**
+ * Converts a card integer value to the cooresponding rank.
+ * 
+ * @param value integer value of the card
+ * @return char cooresponding rank
+ */
+char value_to_rank(int value);
+
+/**
+ * Converts a given amount of the same card into the number pairs it represents.
+ * 
+ * @param count number of instances of the same card
+ * @return int numberr of pairs the count represents
+ */
+int count_to_n_pairs(int count);
+
+/**
+ * Converts a number of card pairs to the cooresponding integer score value representing the play.
+ * 
+ * @param n_pairs number of pairs
+ * @return int score integer value representing the play
+ */
+int n_pairs_to_score(int n_pairs);
+
+/**
+ * Converts a play score to the cooresponding string representation of that play.
+ * 
+ * @param score integer play score
+ * @return char* string representing the play
+ */
+char *score_to_play_string(int score);
+
+/**
+ * Creates a card structure from the given rank and suit.
+ * 
+ * @param rank rank of the card
+ * @param suit suit of the card
+ * @return Card the created card structure
+ */
+Card card_make(char rank, char suit);
+
+/**
+ * Returns an array containing the integer values of the cards contained in a given array of card structures.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return int* array of integer card values
+ */
+int *cards_get_values(size_t card_count, Card *cards);
+
+/**
+ * Returns an array containing the suits of the cards contained in a given of card structures.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return char* array of suit characters
+ */
+char *cards_get_suits(size_t card_count, Card *cards);
+
+/**
+ * Returns an array where the index is the card integer value and the value of the int at a given
+ * index is the number of times that card was contained in the given array of cards.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return int* array of card integer value counts
+ */
+int *cards_get_value_counts(size_t card_count, Card *cards);
+
+/**
+ * Determines the pairs in the given array of cards and returns the cooresponding play structure
+ * using only pair information.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return Play play structure using only pair information
+ */
+Play calc_pairs(size_t card_count, Card *cards);
+
+/**
+ * Return whether or not the given array of card structures is a straight.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return true array of cards contains a straight
+ * @return false array of cards does not contain a straight
+ */
+bool is_straight(size_t card_count, Card *cards);
+
+/**
+ * Return whether or not the given array of card structures is a flush.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return true array of cards contains a flush
+ * @return false array of cards does not contain a flush
+ */
+bool is_flush(size_t card_count, Card *cards);
+
+/**
+ * Return whether or not the given array of card structures is a royal flush.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return true array of cards contains a royal flush
+ * @return false array of cards does not contain a royal flush
+ */
+bool is_royal(size_t card_count, Card *cards);
+
+/**
+ * Returns a play structure representing the best play that an array of cards can make.
+ * 
+ * @param card_count number of cards
+ * @param cards array of cards
+ * @return Play resulting play structure
+ */
+Play calculate_play(size_t card_count, Card *cards);
+
+/**
+ * Compares the high cards between two play structures.
+ * 
+ * @param a left hand side play
+ * @param b right hand side play
+ * @return int negative = a lost, zero = draw, positive = a won
+ */
+int high_vals_cmp(Play *a, Play *b);
+
+/**
+ * Compares the play cards between two play structures.
+ * 
+ * @param a left hand side play
+ * @param b right hand side play
+ * @return int negative = a lost, zero = draw, positive = a won
+ */
+int play_vals_cmp(Play *a, Play *b);
+
+/**
+ * Compares two play structures.
+ * 
+ * @param a left hand side play
+ * @param b right hand side play
+ * @return int negative = a lost, zero = draw, positive = a won
+ */
+int play_cmp(Play *a, Play *b);
+
+int main(int argc, char const *argv[])
+{
+    FILE *fp_in = fopen(POKER_FILE_PATH, "r");
+    if (fp_in == NULL)
+    {
+        printf("Could not open %s for input.", POKER_FILE_PATH);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp_out = fopen(OUTPUT_FILE_PATH, "w");
+    if (fp_out == NULL)
+    {
+        printf("Could not open %s for input.", OUTPUT_FILE_PATH);
+        exit(EXIT_FAILURE);
+    }
+
+    int result = 0;
+
+    char line[STR_BUF_SIZE];
+    while (fgets(line, STR_BUF_SIZE, fp_in))
+    {
+        Card line_cards[10];
+
+        size_t card_idx = 0, char_idx = 0;
+        while (char_idx < strlen(line))
+        {
+            line_cards[card_idx++] = card_make(line[char_idx], line[char_idx + 1]);
+            char_idx += 3;
+        }
+
+        Play player_play = calculate_play(5, &line_cards[0]);
+        Play other_play = calculate_play(5, &line_cards[5]);
+
+        bool player_won = play_cmp(&player_play, &other_play) > 0;
+        result += player_won;
+
+        // formatted printing mess belows
+
+        csis_printf(fp_out, "(Player) ");
+
+        for (size_t i = 0; i < 4; i++)
+            csis_printf(fp_out, "%c ", value_to_rank(line_cards[i].value));
+        csis_printf(fp_out, "%c", value_to_rank(line_cards[5].value));
+
+        csis_printf(fp_out, ", Play = %s", score_to_play_string(player_play.score));
+
+        if (player_play.play_val_count)
+        {
+            csis_printf(fp_out, ", Play cards = ");
+            for (size_t i = 0; i < player_play.play_val_count - 1; i++)
+                csis_printf(fp_out, "%c ", value_to_rank(player_play.play_vals[i]));
+            csis_printf(fp_out, "%c", value_to_rank(player_play.play_vals[player_play.play_val_count - 1]));
+        }
+
+        if (player_play.high_val_count)
+        {
+            csis_printf(fp_out, ", High cards = ");
+            for (size_t i = 0; i < player_play.high_val_count; i++)
+                csis_printf(fp_out, "%c ", value_to_rank(player_play.high_vals[i]));
+        }
+
+        csis_printf(fp_out, "\n");
+
+        csis_printf(fp_out, "(Other)  ");
+
+        for (size_t i = 5; i < 9; i++)
+            csis_printf(fp_out, "%c ", value_to_rank(line_cards[i].value));
+        csis_printf(fp_out, "%c", value_to_rank(line_cards[10].value));
+
+        csis_printf(fp_out, ", Play = %s", score_to_play_string(other_play.score));
+
+        if (other_play.play_val_count)
+        {
+            csis_printf(fp_out, ", Play cards = ");
+            for (size_t i = 0; i < other_play.play_val_count - 1; i++)
+                csis_printf(fp_out, "%c ", value_to_rank(other_play.play_vals[i]));
+            csis_printf(fp_out, "%c", value_to_rank(other_play.play_vals[other_play.play_val_count - 1]));
+        }
+
+        if (other_play.high_val_count)
+        {
+            csis_printf(fp_out, ", High cards = ");
+            for (size_t i = 0; i < other_play.high_val_count; i++)
+                csis_printf(fp_out, "%c ", value_to_rank(other_play.high_vals[i]));
+        }
+
+        csis_printf(fp_out, "\n%s won!\n\n", player_won ? "Player" : "Other");
+    }
+
+    csis_printf(fp_out, "Player won %d times!\n", result);
+
+    fclose(fp_out);
+    fclose(fp_in);
+}
+
 void csis_printf(FILE *fp, const char *formatted_message, ...)
 {
     va_list arglist;
@@ -201,13 +467,6 @@ char *score_to_play_string(int score)
     }
 }
 
-typedef struct
-{
-    char rank;
-    int value;
-    char suit;
-} Card;
-
 Card card_make(char rank, char suit)
 {
     return (Card){
@@ -246,15 +505,6 @@ int *cards_get_value_counts(size_t card_count, Card *cards)
 
     return value_counts;
 }
-
-typedef struct
-{
-    int score;
-    size_t play_val_count;
-    int *play_vals;
-    size_t high_val_count;
-    int *high_vals;
-} Play;
 
 Play calc_pairs(size_t card_count, Card *cards)
 {
@@ -440,98 +690,4 @@ int play_cmp(Play *a, Play *b)
         return play_vals_cmp_result;
 
     return high_vals_cmp(a, b);
-}
-
-int main(int argc, char const *argv[])
-{
-    FILE *fp_in = fopen(POKER_FILE_PATH, "r");
-    if (fp_in == NULL)
-    {
-        printf("Could not open %s for input.", POKER_FILE_PATH);
-        exit(EXIT_FAILURE);
-    }
-
-    FILE *fp_out = fopen(OUTPUT_FILE_PATH, "w");
-    if (fp_out == NULL)
-    {
-        printf("Could not open %s for input.", OUTPUT_FILE_PATH);
-        exit(EXIT_FAILURE);
-    }
-
-    int result = 0;
-
-    char line[STR_BUF_SIZE];
-    while (fgets(line, STR_BUF_SIZE, fp_in))
-    {
-        Card line_cards[10];
-
-        size_t card_idx = 0, char_idx = 0;
-        while (char_idx < strlen(line))
-        {
-            line_cards[card_idx++] = card_make(line[char_idx], line[char_idx + 1]);
-            char_idx += 3;
-        }
-
-        Play player_play = calculate_play(5, &line_cards[0]);
-        Play other_play = calculate_play(5, &line_cards[5]);
-
-        bool player_won = play_cmp(&player_play, &other_play) > 0;
-
-        csis_printf(fp_out, "(Player) ");
-
-        for (size_t i = 0; i < 4; i++)
-            csis_printf(fp_out, "%c ", value_to_rank(line_cards[i].value));
-        csis_printf(fp_out, "%c", value_to_rank(line_cards[5].value));
-
-        csis_printf(fp_out, ", Play = %s", score_to_play_string(player_play.score));
-
-        if (player_play.play_val_count)
-        {
-            csis_printf(fp_out, ", Play cards = ");
-            for (size_t i = 0; i < player_play.play_val_count - 1; i++)
-                csis_printf(fp_out, "%c ", value_to_rank(player_play.play_vals[i]));
-            csis_printf(fp_out, "%c", value_to_rank(player_play.play_vals[player_play.play_val_count - 1]));
-        }
-
-        if (player_play.high_val_count)
-        {
-            csis_printf(fp_out, ", High cards = ");
-            for (size_t i = 0; i < player_play.high_val_count; i++)
-                csis_printf(fp_out, "%c ", value_to_rank(player_play.high_vals[i]));
-        }
-
-        csis_printf(fp_out, "\n");
-
-        csis_printf(fp_out, "(Other)  ");
-
-        for (size_t i = 5; i < 9; i++)
-            csis_printf(fp_out, "%c ", value_to_rank(line_cards[i].value));
-        csis_printf(fp_out, "%c", value_to_rank(line_cards[10].value));
-
-        csis_printf(fp_out, ", Play = %s", score_to_play_string(other_play.score));
-
-        if (other_play.play_val_count)
-        {
-            csis_printf(fp_out, ", Play cards = ");
-            for (size_t i = 0; i < other_play.play_val_count - 1; i++)
-                csis_printf(fp_out, "%c ", value_to_rank(other_play.play_vals[i]));
-            csis_printf(fp_out, "%c", value_to_rank(other_play.play_vals[other_play.play_val_count - 1]));
-        }
-
-        if (other_play.high_val_count)
-        {
-            csis_printf(fp_out, ", High cards = ");
-            for (size_t i = 0; i < other_play.high_val_count; i++)
-                csis_printf(fp_out, "%c ", value_to_rank(other_play.high_vals[i]));
-        }
-
-        csis_printf(fp_out, "\n%s won!\n\n", player_won ? "Player" : "Other");
-
-        result += player_won;
-    }
-
-    csis_printf(fp_out, "Player won %d times!\n", result);
-
-    fclose(fp_out);
-    fclose(fp_in);
 }
